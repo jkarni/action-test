cd26006280dafa4b15e884ff6fc3a5fcc997430d
Julian K. Arni <jkarni@gmail.com>
2021-09-19 22:29:10 +0200

    Fix request, add repo deets

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const request = require('request');
 
 try {
-  const url = core.getInput('url');
-  console.log(`Querying ${url}!`);
-  let request = new Request(url);
-  let result = "success";
-  fetch(request)
-        .then((response) => {
-            if(!response.ok) {
-                response.text().then((t) => core.notice(t));
-                result = "failure";
-                core.setFailed("Server says bad!")
-            } else {
-                response.text().then((t) => core.info(t));
-            }
-        })
-  core.setOutput("result", result);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  github.context.repo.owner
+  const baseUrl = core.getInput('url');
+  const url = baseUrl + github.context.repo.owner + "/"
+         + github.context.repo.repo + "/commit/" + github.context.sha;
+  console.log(`Querying ${url}!`);
+  let result = "success";
+  request.post(url, (err, response, body) => {
+    if(response.statusCode != 200) {
+      core.notice(body);
+      result = "failure";
+      core.setFailed("Server says bad!")
+    } else {
+      response.text().then((t) => core.info(t));
+    }
+  })
+  core.setOutput("result", result);
 } catch (error) {
   core.setFailed(error.message);
 }
